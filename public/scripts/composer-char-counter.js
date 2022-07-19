ac15d63546b3daa68a5d7c965378757e45aba6dd
Mingu Kim <mkim245@gmail.com>
2022-07-19 18:43:20 -0400

    update upto character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log ("document is read");
+  $('#tweet-text').on('input', function (event) {
+    let countChar = $(this).val().length;
+    let countRemainChar = 140 - countChar;
+    console.log(countRemainChar);
+    $( '#countNumber' ).text(function () {
+      if(countRemainChar < 0 ) {
+        $('#countNumber').css("color", "red");
+      } else {
+        $('#countNumber').css("color", "black");
+      }
+      return countRemainChar;
+    });
+});
+})
