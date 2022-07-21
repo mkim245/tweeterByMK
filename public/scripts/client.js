c650b127ce670c3c7c05c266ad3dd9a32981b3b4
Mingu Kim <mkim245@gmail.com>
2022-07-20 20:49:53 -0400

    add fetching tweets, timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 // loops through tweets, calls createTweetElement for each tweet, takes return value and appends it to the tweets container
 const renderTweets = function (tweets) {
@@ -50,7 +50,7 @@ const createTweetElement = function (tweet) {
     </header>
     <p>${tweet.content.text}</p>
     <footer>
-    <span>${tweet.created_at}</span>
+    <span>${tweet.created_at, timeago.format(new Date())}</span>
       <div>
         <span> <i class="fa-solid fa-flag hover"></i> </span>
         <span> <i class="fa-solid fa-retweet hover"></i> </span>
@@ -63,20 +63,35 @@ const createTweetElement = function (tweet) {
 };
 
 
-// const submitTweet = function(event) {
-//   event.preventDefault();
-// })
-
-// }
-
 $(document).ready(function () {
+
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then((tweets) => {
+        renderTweets(tweets);
+      })
+      .catch((err) => {
+        console.log("error", err)
+      });
+  };
+
   $("#newTweetForm").on("submit", function (event) {
     event.preventDefault();
+
     $.ajax('/tweets', {
       method: 'POST',
       data: $(this).serialize()
     })
-  })
-  renderTweets(data);
+      .then((tweets) => {
+        loadTweets();
+      })
+      .catch((err) => {
+        console.log("error", err)
+      });
+  });
+
+  loadTweets();
+
 });
 
+  // renderTweets(data);
