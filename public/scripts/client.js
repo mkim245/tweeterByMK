726fa0db5246303cba298ccdaec7e250bf875fc2
Mingu Kim <mkim245@gmail.com>
2022-07-20 21:47:13 -0400

    add validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 // loops through tweets, calls createTweetElement for each tweet, takes return value and appends it to the tweets container
 const renderTweets = function (tweets) {
   for (let tweet of tweets) {
@@ -78,6 +53,13 @@ $(document).ready(function () {
   $("#newTweetForm").on("submit", function (event) {
     event.preventDefault();
 
+    if ($('#tweet-text').val().length === 0) {
+      return alert('Your tweet is empty')
+    }
+    if ($('#tweet-text').val().length > 140) {
+      return alert('Your tweet is longer than 140 characters')
+    }
+
     $.ajax('/tweets', {
       method: 'POST',
       data: $(this).serialize()
@@ -93,5 +75,3 @@ $(document).ready(function () {
   loadTweets();
 
 });
-
-  // renderTweets(data);
